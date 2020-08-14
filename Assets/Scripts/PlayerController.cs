0fa3a127bcae8a40c455cfc46e5609dcd6c64ffd
Reid <reid.braun@me.com>
2020-08-14 16:42:02 -0500

    Added background movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,19 +9,34 @@ public class PlayerController : MonoBehaviour
 
     public int jumpForce;
 
+    public Transform groundPoint;
+
+    public LayerMask groundLayer;
+
+    public bool grounded;
+
     // Start is called before the first frame update
     void Start()        
     {
         
     }
 
+    //Update method used for physics in our game
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapPoint(groundPoint.position,groundLayer);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (grounded && Input.GetKeyDown(KeyCode.Space)) 
         {
             anim.SetTrigger("Jump");
             rb.AddForce(Vector2.up * jumpForce);
         }
+
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("Grounded", grounded);
     }
 }
