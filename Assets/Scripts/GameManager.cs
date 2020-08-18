072adc8464df498ccdb1ed92d4894c40aa13bce0
Reid <reid.braun@me.com>
2020-08-18 15:37:25 -0500

    Added coins

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int currentScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore()
+    {
+        currentScore++;
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+}
